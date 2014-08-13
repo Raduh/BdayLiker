77e373bf4c39cea6eb783e243e3d8c0d2dc560af
Radu Hambasan <radu.hambasan@gmail.com>
2014-08-13 17:07:40 +0200

    Finish script

--- liker.rb
+++ liker.rb
@@ -4,7 +4,13 @@ require './util.rb'
 require 'json'
 require 'koala'  # Facebook API
 
-SECRET_PATH = "./.env.json"
+# Config
+SECRET_PATH = "./env.json"
+MAX_TO_PROCESS = 1000  # maximum number of posts we expect to process
+BIRTHDAY_DAY = 13  # 1..31
+BIRTHDAY_MONTH = 8  # 1..12
+
+
 if !(File.exists? SECRET_PATH)
     abort("No env file (with the access token).".red)
 end
@@ -17,8 +23,36 @@ end
 ACCESS_TOKEN = env_json['access_token']
 $graph = Koala::Facebook::API.new(ACCESS_TOKEN)
 
-FQL_QUERY = "SELECT post_id, actor_id, target_id, created_time, message, comments FROM stream 
-WHERE source_id = me() AND filter_key='others' AND actor_id!=me() AND created_time > 1407275080"
-q_results = $graph.fql_query(FQL_QUERY)
+# getting a timestamps before and after the birthday
+current_year = Time.now.year
+tstamp_before = Time.new(current_year, BIRTHDAY_MONTH, BIRTHDAY_DAY - 1, 12, 0).to_i
+tstamp_after = Time.new(current_year, BIRTHDAY_MONTH, BIRTHDAY_DAY + 1, 12, 0).to_i
 
+FQL_QUERY =
+    "SELECT source_id, actor_id, post_id, created_time, message "\
+    "FROM stream "\
+    "WHERE source_id = me() "\
+        "AND filter_key = 'others' "\
+        "AND actor_id != me() "\
+        "AND created_time > #{tstamp_before} "\
+        "AND created_time < #{tstamp_after} "\
+    "LIMIT #{MAX_TO_PROCESS}"
+
+begin
+    q_results = $graph.fql_query(FQL_QUERY)
+rescue Koala::Facebook::AuthenticationError
+    abort("Authentication token is not valid. Maybe expired?").red
+end
+
+puts "#{q_results.length} posts to process".green.bold
+
+for postJSON in q_results
+    post_id = postJSON['post_id']
+    $graph.put_like(post_id)
+    sleep 0.2;  # we are not bots, so we sleep 
+    puts "LIKE: ".green + postJSON['message']
+    $stdout.flush
+end
+
+puts "Done.".bold.green
 
